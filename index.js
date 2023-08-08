575776c00aa2b1ae6e69925900f800f1d4a575e7
buddhika4u <buddhika4u98@gmail.com>
2023-08-08 16:34:08 +0530

    Connect mongodb cloud database

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('./config'); // Import the MongoDB URI from config.js
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(err => console.error('Error connecting to MongoDB:', err));
